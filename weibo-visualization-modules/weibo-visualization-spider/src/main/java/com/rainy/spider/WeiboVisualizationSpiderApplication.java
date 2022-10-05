902fff0f37857e4cbff7d4a303a0b16de1b28a4c
rainysteven1 <rainysteven1@gmail.com>
2022-10-05 18:47:36 +0800

    项目初始化

---
+++ weibo-visualization-modules/weibo-visualization-spider/src/main/java/com/rainy/spider/WeiboVisualizationSpiderApplication.java
@@ -0,0 +1,13 @@
+package com.rainy.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiboVisualizationSpiderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiboVisualizationSpiderApplication.class, args);
+    }
+
+}
